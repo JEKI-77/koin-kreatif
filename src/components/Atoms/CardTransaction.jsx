5892aaf5330f5d19695dc8c33a6d97fd37e41441
Jeki Saputra <saputrajeki90@gmail.com>
2024-03-23 20:14:28 +0700

    integrasi api: get dan post method

--- src/components/Atoms/CardTransaction.jsx
+++ src/components/Atoms/CardTransaction.jsx
@@ -1,20 +1,24 @@
 import { FaEdit } from "react-icons/fa";
 import { RiDeleteBin5Fill } from "react-icons/ri";
 import { IoMdDownload } from "react-icons/io";
+import { MdUpload } from "react-icons/md";
 // import { MdUpload } from "react-icons/md";
 
 // eslint-disable-next-line react/prop-types
-const CardTransaction = ({ amount }) => {
+const CardTransaction = ({ amount, category, status, }) => {
   return (
     <div className="bg-zinc-300 py-2  flex justify-between rounded-md px-4 ">
       <div className="flex gap-4">
-        <div className="flex justify-center items-center">
-          <IoMdDownload />
-          {/* <MdUpload /> */}
+        <div
+          className={`flex justify-center items-center ${
+            status == false ? "text-red-600" : "text-green-600 "
+          } `}
+        >
+          {status == false ? <MdUpload /> : <IoMdDownload />}
         </div>
         <div>
           <h1>{amount}</h1>
-          <span>pendidikan</span>
+          <span>{category}</span>
         </div>
       </div>
       <div className="flex gap-8 justify-center items-center">
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,35 @@
 // import DatePicker from "react-horizontal-datepicker";
 // import ReactDatePicker from "react-datepicker";
+import { useEffect } from "react";
 import Card from "../../components/Atoms/Card";
 import CardTransaction from "../../components/Atoms/CardTransaction";
 import "react-datepicker/dist/react-datepicker.css";
+import axios from "axios";
+import { useState } from "react";
 // import { useState } from "react";
 
 const Home = () => {
-  // const [startDate, setStartDate] = useState(new Date());
-  // console.log(startDate);
+  const [data, setData] = useState([]);
+  console.log(data);
+
+  const fetchdata = async () => {
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_APP_URL}/v1/transactions`
+      );
+      setData(response.data);
+      console.log("data", response);
+    } catch (error) {
+      console.log(error);
+      alert("create gagal", error);
+      // Logika untuk msenangani kesalahan
+    }
+  };
+
+  useEffect(() => {
+    fetchdata();
+  }, []);
+
   return (
     <div className="w-[90%] md:w-[40%]">
       <div className=" mt-4 flex justify-center items-center">
@@ -19,23 +41,18 @@ const Home = () => {
       </div>
 
       <Card />
-      <div className="flex flex-col gap-4 mt-8 no-scrollbar overflow-y-auto h-[25em] md:h-[25em] ">
-        <CardTransaction amount="20000" />
-        <CardTransaction amount="20000" />
-        <CardTransaction amount="20000" />
-        <CardTransaction amount="20000" />
-        <CardTransaction amount="20000" />
-        <CardTransaction amount="20000" />
-        <CardTransaction amount="20000" />
-        <CardTransaction amount="20000" />
-        <CardTransaction amount="20000" />
-        <CardTransaction amount="20000" />
-        <CardTransaction amount="20000" />
-        <CardTransaction amount="20000" />
-        <CardTransaction amount="20000" />
-        <CardTransaction amount="20000" />
-        <CardTransaction amount="20000" />
-        <CardTransaction amount="20000" />
+      <div>
+        {Array.isArray(data) &&
+          data.map((item) => (
+            <div className="my-4" key={item.key}>
+              <CardTransaction
+                amount={item.amount}
+                category={item.category}
+                status={item.status}
+                
+              />
+            </div>
+          ))}
       </div>
     </div>
   );
