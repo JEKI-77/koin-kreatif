356cf2fbac0e7127e0a0343b4de5665c91a26074
Jeki Saputra <saputrajeki90@gmail.com>
2024-03-29 10:25:32 +0700

    revised

--- src/pages/Transaction/AddTransaction.jsx
+++ src/pages/Transaction/AddTransaction.jsx
@@ -164,7 +164,7 @@ const AddTransaction = () => {
                     <div className="col-span-2 sm:col-span-1">
                       <label
                         htmlFor="price"
-                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                        className="block mb-2 text-sm font-medium  text-gray-900 dark:text-white"
                       >
                         date
                       </label>
@@ -174,7 +174,7 @@ const AddTransaction = () => {
                         type="date"
                         name="date"
                         id="date"
-                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 h-10 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                         placeholder="date"
                         required=""
                       />
@@ -193,7 +193,7 @@ const AddTransaction = () => {
                         value={category}
                         onChange={(e) => setCategory(e.target.value)}
                         id="category"
-                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                        className="bg-gray-50 h-10 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                       >
                         <option value="">Select category</option>
                         {categoryData.map((info, index) => (
