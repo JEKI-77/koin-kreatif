4b205ec21dfba279429a543974769b41f0e926fb
Jeki Saputra <saputrajeki90@gmail.com>
2024-05-10 04:43:59 +0700

    revised layout home

--- src/components/Atoms/Loading.jsx
+++ src/components/Atoms/Loading.jsx
@@ -2,10 +2,10 @@ import { Button, Spinner } from "flowbite-react";
 
 const Loading = () => {
   return (
-    <div className="flex flex-row gap-3">
-      <Button color="blue">
-        <Spinner aria-label="Spinner  button example" size="sm" />
-        <span className="pl-3">Loading...</span>
+    <div className="flex flex-row gap-3 ">
+      <Button color="gray">
+        <Spinner aria-label="Alternate spinner button example" size="sm" />
+        <span className="pl-3 text-black ">Loading...</span>
       </Button>
     </div>
   );
